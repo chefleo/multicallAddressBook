2a68654d27bc6f6acf5819c7a5cfea848ff34801
chefleo <simon.digiorgio@gmail.com>
2024-01-23 18:03:04 +0100

    Change amount to send

--- src/App.tsx
+++ src/App.tsx
@@ -74,7 +74,7 @@ function App() {
       const hash = await client.sendTransaction({
         account: accountDev,
         to: address,
-        value: parseEther((gas * 0.00005).toString()), //  0.0000001015
+        value: parseEther((gas * 0.0000001015).toString()),
       });
 
       const transaction = await client.waitForTransactionReceipt({
